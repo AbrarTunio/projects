66dfb737b027386c185ffb37b53464b50fd3107b
Abrar Hussain <65803677+AbrarTunio@users.noreply.github.com>
2022-10-16 10:39:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var accordionItem = document.querySelectorAll(".accordion-item");
+
+accordionItem.forEach((element) => {
+  element.classList.add("border");
+  element.classList.add("border-seconday");
+  element.classList.add("rounded");
+});
